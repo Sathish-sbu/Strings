746ab6917e72725c0636e9e4e5dc257452e8bb91
Sathish Ejoumale <35587913+Sathish-sbu@users.noreply.github.com>
2019-03-21 02:46:45 -0400

    numberOfLines.py

---
+++ numberOfLines.py
@@ -0,0 +1,26 @@
+"""
+We are to write the letters of a given string S, from left to right into lines. Each line has maximum width 100 units, and if writing a letter would cause the width of the line to exceed 100 units, it is written on the next line. We are given an array widths, an array where widths[0] is the width of 'a', widths[1] is the width of 'b', ..., and widths[25] is the width of 'z'.
+
+Now answer two questions: how many lines have at least one character from S, and what is the width used by the last such line? Return your answer as an integer list of length 2.
+"""
+class Solution(object):
+    def numberOfLines(self, widths, S):
+        """
+        :type widths: List[int]
+        :type S: str
+        :rtype: List[int]
+        """
+        
+        
+        valSum = 0
+        lines = 1
+            
+        for s in S:
+            curr = widths[ord(s) - 97]
+            if valSum + curr <= 100:
+                valSum += curr
+            else:
+                lines += 1
+                valSum = curr
+            
+        return lines, valSum
